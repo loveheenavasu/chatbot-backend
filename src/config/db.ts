f91e73141f9233b963e9d8bfa66de65668f66516
anita <anita.js@zestgeek.com>
2024-07-19 18:40:49 +0530

    mongodb

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
 import { config } from 'dotenv';
 config();
-const { DB_HOST, DB_NAME, DB_PORT } = process.env;
-
+// const { DB_HOST, DB_NAME, DB_PORT } = process.env;
+const { URI } = process.env;
+const uri: any = URI
 const dbConnect = async () => {
     try {
-        const URI = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
-        mongoose.connect(URI);
+        // const URI = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+        mongoose.connect(uri);
     }
     catch (err) {
         throw err;
